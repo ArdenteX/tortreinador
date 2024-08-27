c4f9ed327d1e0f4d3e57744a72312ab9a8252ed4
ArdenteX <694022879@qq.com>
2024-08-28 05:45:32 +0800

    2024/08/28

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('tortreinador/'))
-
+sys.path.insert(0, os.path.abspath('../..'))
+print("sys.path:", sys.path)
 
 project = 'Tortreinador'
 copyright = '2024, Xavier'
